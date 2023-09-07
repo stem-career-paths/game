57327ed18b82c53a5ba3ae0206167d4f25200e67
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-09-07 11:32:25 -0400

    Add gut unit testing addon
    It has been configured with my preferred settings. There is
    a sample test case to ensure the configuration is correct.

---
+++ project/test/unit/gut_configuration_test.gd
@@ -0,0 +1,7 @@
+## Tests that the Gut configuration is correct.
+##
+## This file should be removed when real tests are put in place.
+extends GutTest
+
+func test_test_runs():
+	assert_true(true, "Gut configuration is fine.")
