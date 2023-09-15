6548098d4223ab7ac59b30c509e5e278fcc15c2c
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2023-09-15 18:10:17 -0400

    Add initial story effect change tracker

--- project/story/stateful_demo.gd
+++ project/story/stateful_demo.gd
@@ -5,9 +5,23 @@ var _count := 0
 func run(presenter)->void:
 	while _count < 2:
 		presenter.show_text("You have chosen %d options"% _count)
-		await presenter.show_options(["Choose Something"])
+		var _options := [
+			"Choose Something",
+			"Choose Something Else",
+		]
+
+		var _choice = _options.find(await presenter.show_options(_options))
 		_count += 1
-	
+		
+		match _choice:
+			0:
+				presenter.show_effects({
+					"mathematics": _count,
+				})
+			1:
+				presenter.show_effects({
+					"resilience": _count,
+				})
+
 	presenter.show_text("You have chosen enough. Go home.")
 	await presenter.show_confirmation()
-	print("Sample story complete.")
