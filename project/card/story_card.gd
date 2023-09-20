d2538bb846a9bef7b787a87660991901a58b9f65
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-09-20 14:53:48 -0400

    Animate the entry of new cards
    Drop shadows would really help here, but that's a problem for the
    future. This is a proof of concept with a simple tween.

---
+++ project/card/story_card.gd
@@ -0,0 +1,63 @@
+extends Control
+
+var world : World:
+	set(value):
+		world = value
+		%CharacterDisplay.character = world.character
+
+signal _option_selected(option: String)
+
+
+@onready var _button_area := %ButtonArea
+@onready var _main_image :TextureRect = %MainImage
+
+func _ready():
+	assert(world!=null)
+	
+
+func show_confirmation(text := "OK") -> void:
+	# Remove any leftover buttons from the screen before showing the 
+	# confirmation button
+	_clear(_button_area)
+	
+	var button := Button.new()
+	button.text = text
+	_button_area.add_child(button)
+	await button.pressed
+	button.disabled = true
+
+
+func show_effects(effects: Dictionary) -> void:
+	for attribute in effects.keys():
+		world.character[attribute] += effects[attribute]
+
+
+func show_options(options: Array) -> String:
+	var buttons : Array[Button] = []
+	for option in options:
+		var button := Button.new()
+		button.text = option
+		button.pressed.connect(func(): _option_selected.emit(option))
+		_button_area.add_child(button)
+		buttons.append(button)
+	
+	var selection = await _option_selected
+	
+	for button in buttons:
+		button.disabled = true
+	
+	return selection
+
+
+func show_text(text: String) -> void:
+	%Text.text = text
+
+
+func show_image(image: String) -> void:
+	if image!=null:
+		_main_image.texture = load("res://images/%s.png" % image)
+
+
+func _clear(container:Control) -> void:
+	while container.get_child_count() > 0:
+		container.remove_child(container.get_child(0))
--- project/ui/main_screen.gd
+++ project/ui/main_screen.gd
@@ -1,57 +1,45 @@
 extends Control
 
-var world := World.new()
-
 signal _option_selected(option: String)
 
+enum _EntranceType { FIXED, ANIMATED }
+
+@export_file("*.gd") var initial_story 
+@export var slide_in_duration := 0.35
+
+var world := World.new()
+var _max_x := 750
+
+var _current_card : Node
 
 func _ready():
-	%CharacterDisplay.character = world.character
-	var story := preload("res://story/meta_story.gd").new()
-	await story.run(self)
-	print("Story is done")
+	assert(initial_story!=null)
+	var story = load(initial_story).new()
+	await _show_card(story, _EntranceType.FIXED)
+	
+	var next = load("res://story/steven_dnd.gd").new()
+	await _show_card(next, _EntranceType.ANIMATED)
 
 
-func _clear_story_area():
-	for child in %StoryArea.get_children():
-		child.queue_free()
+func _show_card(story : Object, entrance : _EntranceType) -> void:
+	var presenter := preload("res://card/story_card.tscn").instantiate()
+	presenter.world = world
+	
+	if entrance == _EntranceType.ANIMATED:
+		presenter.position.x = _max_x
+	
+	add_child(presenter)
+	
+	if entrance == _EntranceType.ANIMATED:
+		var tween := get_tree().create_tween()
+		var previous_card = _current_card
+		tween.tween_property(presenter, "position", Vector2.ZERO, slide_in_duration)\
+			.set_ease(Tween.EASE_IN)
+		# Remove the card in the background once it is covered up
+		tween.tween_callback(func(): 
+			if previous_card != null:
+				remove_child(previous_card)
+		)
 
-
-## PRESENTER METHODS ------------------------------------
-
-
-func show_confirmation(text := "OK") -> void:
-	var button := Button.new()
-	button.text = text
-	%StoryArea.add_child(button)
-	await button.pressed
-
-	_clear_story_area()
-
-
-func show_effects(effects: Dictionary) -> void:
-	for attribute in effects.keys():
-		world.character[attribute] += effects[attribute]
-
-
-func show_options(options: Array) -> String:
-	for option in options:
-		var button := Button.new()
-		button.text = option
-		button.pressed.connect(func(): _option_selected.emit(option))
-		%StoryArea.add_child(button)
-
-	var selection = await _option_selected
-	print("Going to return ", selection)
-
-	_clear_story_area()
-
-	return selection
-
-
-func show_text(text: String) -> void:
-	%Text.text = "[center]%s" % text
-
-
-func show_image(image: String) -> void:
-	%Image.texture = load("res://images/%s.png" % image)
+	_current_card = presenter
+	await story.run(presenter)
