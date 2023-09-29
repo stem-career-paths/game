c1a8df5db836a07d3462a08e230fcb73e4221fe1
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2023-09-29 14:20:40 -0400

    Add array[String] in text support to SimpleStory
    Signed-off-by: Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>

--- project/card/simple_story.gd
+++ project/card/simple_story.gd
@@ -1,17 +1,36 @@
 class_name SimpleStory extends RefCounted
 
-func run(presenter)->void:
+
+func run(presenter) -> void:
 	assert("text" in self, "This story must have 'text'")
+	assert(get("text") is String or get("text") is Array, "The 'text' must be a string or an array")
 	assert("options" in self, "This story must have 'options'")
-	
-	presenter.show_text(get("text"))
+
 	if "npc_name" in self:
-		var npc : Npc = presenter.world.cast.get_by_name(get("npc_name"))
+		var npc: Npc = presenter.world.cast.get_by_name(get("npc_name"))
 		presenter.show_npc(npc)
-	
+
+	await _show(presenter, get("text"), get("effects"))
+
 	var options_dict = get("options")
 	var option_keys = options_dict.keys()
 	var selected = await presenter.show_options(option_keys)
-	presenter.show_text(options_dict[selected].text)
-	await presenter.show_effects(options_dict[selected].effects)
+
+	await _show(presenter, options_dict[selected].text, options_dict[selected].effects)
+
 	await presenter.show_confirmation()
+
+
+func _show(presenter, text, effects = null):
+	if text is String:
+		presenter.show_text(text)
+	else:
+		presenter.show_text(text[0])
+
+	if effects is Dictionary:
+		await presenter.show_effects(effects)
+
+	if text is Array:
+		for line in text.slice(1):
+			await presenter.show_confirmation()
+			presenter.show_text(line)
