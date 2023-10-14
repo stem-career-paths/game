81b8b402352c0025dc26269e8c0e17291bd0e799
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2023-10-14 10:05:16 -0400

    Add slight variation to audible button click sound

--- project/ui/audible_button.gd
+++ project/ui/audible_button.gd
@@ -1,4 +1,12 @@
 extends Button
 
+@onready var _audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
+
+func play_varied_sound():
+	_audio_stream_player.pitch_scale = randf_range(0.9, 1.1)
+	_audio_stream_player.play()
+
+
 func _on_pressed():
-	$AudioStreamPlayer.play()
+	play_varied_sound()
