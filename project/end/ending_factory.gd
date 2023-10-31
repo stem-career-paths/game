07a00acc0178ea14eea3c33638909c8c1224aa3a
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2023-10-31 08:41:37 -0400

    Update ending factory

--- project/end/ending_factory.gd
+++ project/end/ending_factory.gd
@@ -3,24 +3,24 @@ extends RefCounted
 # According to the National Center for Education Statistics, in 2019 (the
 # last prepandemic year), 66% of high school graduates when directly into
 # higher education. This can be broken down into 22% into two-year schools
-# and 44% into four-year schools. 
+# and 44% into four-year schools.
 #
 # See https://nces.ed.gov/fastfacts/display.asp?id=51
 #
 # These constants assume there's a 90% chance the student is going to college.
 # That is completely arbitrary. The rest of the constants are computed from that
 # given the data above.
-const _HIGHER_ED_PROBABILITY := 0.9
+const HIGHER_ED_PROBABILITY := 0.9
 
 ## Probability of going to 2-year college vs 4-year college
 ##
 ## This is computed by the ratio reported by NCES in 2019.
-const _COMMUNITY_COLLEGE_PROBABILITY := 0.22/0.66
+const COMMUNITY_COLLEGE_PROBABILITY := 0.22/0.66
 
-const _END_STORY_PATH := "res://end/"
+const END_STORY_PATH := "res://end/"
 
-const _MAJORS_BY_ATTRIBUTE := {
-	"science" : [
+const MAJORS_BY_ATTRIBUTE := {
+	"science": [
 		"astronomy",
 		"biology",
 		"chemistry",
@@ -47,45 +47,59 @@ const _MAJORS_BY_ATTRIBUTE := {
 	],
 }
 
-func make_ending_story(world : World) -> String:
-	if world.end_stories.size() > 0:
+
+func make_ending_story(world: World) -> String:
+	if not world.end_stories.is_empty():
 		return _pick_random_canned_end_story(world)
-	else:
-		return _generate_random_ending(world)
+
+	return _generate_random_ending(world)
 
 
-func _pick_random_canned_end_story(world : World) -> String:
+func _pick_random_canned_end_story(world: World) -> String:
 	assert(not world.end_stories.is_empty())
 	var end_story_name := world.end_stories[randi() % world.end_stories.size()]
-	var end_story = load("%send_%s.gd" % [_END_STORY_PATH, end_story_name]).new()
+	var end_story = load("%send_%s.gd" % [END_STORY_PATH, end_story_name]).new()
 	return end_story.text
 
 
-func _generate_random_ending(world : World) -> String:
+func _generate_random_ending(world: World) -> String:
 	var result := ""
 
-	if randf() > _HIGHER_ED_PROBABILITY:
+	if randf() > HIGHER_ED_PROBABILITY:
 		result += _generate_gap_year_text() + " "
-	
-	if randf() <= _COMMUNITY_COLLEGE_PROBABILITY:
+
+	if randf() <= COMMUNITY_COLLEGE_PROBABILITY:
 		result += "You go to community college %s " % [
 			"and earn an associate's degree. Then, you go ",
 			"to complete some core courses like writing and global studies. Then, you transfer "
 		].pick_random()
-		result += "to %s" % [
-			"a state university",
-			"a state college",
-		].pick_random()
+		result += "to %s"
 	else:
-		result += "You attend %s" % [
-			"a small liberal arts college",
-			"a state university",
-			"a state college"
-		].pick_random()
-	
+		result += "You attend %s"
+
+	result = result % _generate_college_text()
 	result += " and major in %s." % _pick_major(world.character).capitalize()
 
 	return result
+
+
+func _generate_college_text() -> String:
+	var size: String = [
+		"small",
+		"medium",
+		"large",
+	].pick_random()
+
+	var option: String = [
+		"liberal arts college",
+		"state university",
+		"state college",
+	].pick_random()
+
+	return "a {size} {option}".format({
+		"size": size,
+		"option": option,
+	})
 
 
 func _generate_gap_year_text() -> String:
@@ -99,10 +113,9 @@ func _generate_gap_year_text() -> String:
 	].pick_random()
 
 
-func _pick_major(character : Character) -> String:
-	var keys : Array[String] = []
-	keys.assign(_MAJORS_BY_ATTRIBUTE.keys())
-	var highest_attribute_name : String = \
+func _pick_major(character: Character) -> String:
+	var keys: Array[String] = []
+	keys.assign(MAJORS_BY_ATTRIBUTE.keys())
+	var highest_attribute_name: String = \
 		character.get_highest_attribute_names(keys).pick_random()
-	return _MAJORS_BY_ATTRIBUTE[highest_attribute_name].pick_random() as String
-	
+	return MAJORS_BY_ATTRIBUTE[highest_attribute_name].pick_random() as String
