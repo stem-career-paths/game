2b1568e1d08abf8405a7df919d6f057a0eb0421f
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-11-02 09:49:52 -0400

    Add robotics path stories

---
+++ project/story/robotics_club/parent_influence.gd
@@ -0,0 +1,31 @@
+extends SimpleStory
+
+var text := [
+	'"I have been thinking about college. My dad is an engineer. He designs electronics systems for factories."',
+	'"I have always thought that sounded good, but I also like creative writing. What should I look for in a major?"',
+]
+
+var npc_name := "Aubrey"
+
+var options := {
+	"Do whatever you want": {
+		"text": '"Sure, but I am not sure what that is."',
+	},
+	"Engineers make good money": {
+		"text": '"Yeah, I guess I should think about what I want to do and what will be a good career path."',
+		"effects": {
+			"engineering": +1,
+			"engagement": +1,
+		}
+	},
+	"You can do a lot with English": {
+		"text": [
+			'"That\'s what I think!"', 
+			'"My dad works with a lot of technical writers, who write stuff that normal people can read."',
+			'"Maybe that is a good combination of technology and communication for me."'
+		],
+		"effects": {
+			"curiosity": +1,
+		}
+	}
+}
---
+++ project/story/robotics_club/programming.gd
@@ -0,0 +1,31 @@
+extends SimpleStory
+
+var text := [
+	'"Looks like someone is going to have to program the robot for the club."',
+	'"I\'ve been programming since I was a kid. I guess I could do it."'
+]
+
+var npc_name := "Aubrey"
+
+var options := {
+	"Do it! You will get even better.": {
+		"text": '"Yeah, I suppose so. Maybe I can be an engineer or a programmer or something in college."',
+		"effects": {
+			"engineering": +1,
+			"engagement": +1,
+		}
+	},
+	"Only if you want to.": {
+		"text": '"Oh, man, I can\'t decide. Part of me wants to, but part wants to try something new."',
+		"effects": {
+			"curiosity": +1
+		}
+	},
+	"No, try something new.": {
+		"text": "I guess this is my chance, right? Maybe I will try using the 3D printer or CNC machine.",
+		"effects": {
+			"curiosity": +1,
+			"engineering": +1
+		}
+	}
+}
