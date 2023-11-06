b0583727a61019a870d6e0cd3957af71485a8352
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-11-06 16:47:39 -0500

    Add athletic scholarship story
    This does not have a lot of real effects, but it seems like a good,
    realistic, human story.

---
+++ project/story/starting_stories/athletic_scholarship.gd
@@ -0,0 +1,20 @@
+extends SimpleStory
+
+var npc_name := "Mary"
+
+var text := '"I would love to go to college and play soccer for a university team. Do you think I could do it?"'
+
+var options := {
+	"Give it 110%": {
+		"text": '"I never understood that phrase. Isn\'t 100% enough?"',
+		"effects": {
+			"resilience": +1
+		}
+	},
+	"You could get a scholarship": {
+		"text": '"Yeah! I am hoping to get a scholarship so I can afford college. It is expensive."',
+	},
+	"Do you want to be a professional athlete?": {
+		"text": "No, I love sports and want to keep playing though. I think I\'d like to be a coach."
+	}
+}
