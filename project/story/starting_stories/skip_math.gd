9155856d5591dbd14a919afd831b5c3b7dc840bd
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-11-06 17:04:23 -0500

    Include a story about skipping taking math
    I did not have room to add a button about death rays. This points
    to a need to have some kind of test around how much text buttons
    can have, since I was bumping out the screen.

---
+++ project/story/starting_stories/skip_math.gd
@@ -0,0 +1,26 @@
+extends SimpleStory
+
+var text := '"I have enough math to graduate. I think I will just skip taking it next semester."'
+
+var options := {
+	"You can\'t know too much!": {
+		"text": '"Yeah, I guess then no matter what I do in college, I will be ready."',
+		"effects": {
+			"mathematics": +1,
+			"engagement": +1,
+		}
+	},
+	"Math is interesting": {
+		"text": '"I didn\'t think about it that way. Probably? I have to think about it."',
+		"effects": {
+			"mathematics": +1,
+			"curiosity": +1
+		}
+	},
+	"You need math to do a lot of things in science.": {
+		"text": '"Yeah, I guess if I wanted to study science, I should probably take more math."',
+		"effects": {
+			"science": +1,
+		}
+	}
+}
