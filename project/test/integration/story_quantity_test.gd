59f9c45a507c34a7dad1ae4a6e41d8509f1e4e6c
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-11-06 16:55:03 -0500

    Remove unclear stories
    These two stories have been around for a while, but I do not
    think that they capture what the game is about. They are giving
    great rewards for nominal interactions. Since there are enough
    other stories, I am removing them for now.

---
+++ project/test/integration/story_quantity_test.gd
@@ -0,0 +1,19 @@
+extends GutTest
+
+const STARTING_STORY_DIR := "res://story/starting_stories/"
+
+func test_there_are_stories_enough_to_fill_a_playthrough() -> void:
+	var world := World.new()
+	var dir := DirAccess.open(STARTING_STORY_DIR)
+	var files := dir.get_files()
+	var count := 0
+	for file_name in files:
+		if file_name.ends_with(".gd"):
+			count += 1
+	
+	var needed_stories := world.turns_per_year * 4
+	
+	assert_true(count >= needed_stories, "Not enough stories. We need at least %d but there are %d." % [
+		world.turns_per_year,
+		needed_stories
+	])
