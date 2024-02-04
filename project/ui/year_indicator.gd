24c8745c7223188b9b1428674cfdb7412b29e98e
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2024-02-03 22:25:14 -0500

    Add confetti on year transition
    Not sure if this is the best way to handle year progression
    feedback. I'll try some different options to see what works
    best.

--- project/ui/year_indicator.gd
+++ project/ui/year_indicator.gd
@@ -4,15 +4,20 @@ var world: World:
 	set(value):
 		if world != null:
 			if world != value:
-				world.years_changed.disconnect(_update_display)
+				world.years_changed.disconnect(_year_progressed)
 
 		world = value
-		world.years_changed.connect(_update_display)
+		world.years_changed.connect(_year_progressed)
 		_update_display(world.years)
 
 @onready var _year_name: Label = %YearName
-@onready var _year_circles : Array[ProgressCircle] = [%Year1, %Year2, %Year3, %Year4 ]
+@onready var _year_circles : Array[ProgressCircle] = [ %Year1, %Year2, %Year3, %Year4 ]
+@onready var _particles: CPUParticles2D = %Particles
 
+
+func _year_progressed(years: int) -> void:
+	_particles.emitting = true
+	_update_display(years)
 
 func _update_display(years: int) -> void:
 	_year_circles[years].year_passed = true
