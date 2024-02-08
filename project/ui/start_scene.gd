aa1745a445ca60c6517eb3d7212791322110f20b
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-02-08 08:35:26 -0500

    Clarify privacy of exported variable
    Exported variables should not be private, of course. That's a contradiction.

--- project/ui/start_scene.gd
+++ project/ui/start_scene.gd
@@ -1,7 +1,7 @@
 extends Control
 
 
-@export var _scroll_speed: float = 300.0
+@export var scroll_speed: float = 300.0
 
 var _index = 0
 
@@ -23,7 +23,7 @@ func _ready() -> void:
 
 
 func _process(delta) -> void:
-	_image_scroll_container.scroll_horizontal += delta * _scroll_speed
+	_image_scroll_container.scroll_horizontal += delta * scroll_speed
 
 	if _image_scroll_container.scroll_horizontal >= 720:
 		_swap_children_images()
