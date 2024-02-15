2924f7eeafe10f495265b87461c67987ce37031f
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-02-15 09:30:29 -0500

    Round the bottom blue inner borders
    This cleans up the edges. The STEMERC block was moved to give it
    a little more room away from the curved edge. The math story had
    to be updated to make it fit.

--- project/story/starting_stories/skip_math.gd
+++ project/story/starting_stories/skip_math.gd
@@ -10,20 +10,20 @@ var options := {
 			"engagement": +1,
 		}
 	},
-	"Math is interesting": {
+	"Math is interesting.": {
 		"text": '"I didn\'t think about it that way. Probably? I have to think about it."',
 		"effects": {
 			"mathematics": +1,
 			"curiosity": +1
 		}
 	},
-	"You need math to do a lot of things in science.": {
+	"You need math to do science.": {
 		"text": '"Yeah, I guess if I wanted to study science, I should probably take more math."',
 		"effects": {
 			"science": +1,
 		}
 	},
-	"Math is stupid. Don\'t take it.": {
+	"Math is stupid.": {
 		"text": [
 			'"[b]Hey[/b]! Not taking it doesn\'t mean I don\'t like it!"',
 			'"I think I\'ll take my math class out of [i]spite[/i]."'
