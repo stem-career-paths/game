3a5db67ad3ac8a0cd965503e671ef1c007ad20c8
Brooke Wright <brookerwright2@gmail.com>
2024-02-28 10:19:01 -0500

    Add a new starting story

---
+++ project/story/starting_stories/asking_questions.gd
@@ -0,0 +1,46 @@
+extends SimpleStory
+
+var text := [
+	"You find Mary lost in thought just outside the classroom door.",
+	'"Oh! Hey! Maybe you can help me. I want to know more about engineering, but when I look it up, it doesn\'t make sense. What should I ask the teacher?"'
+]
+
+var npc_name := "Mary"
+
+var location := "hallway"
+
+var options := {
+	"What do engineers do?": {
+		"text": [
+			'You and Mary go in together and ask the teacher "What do engineers do?"',
+			"The teacher tells you that they can do many things, but they all invent and design solutions to problems."
+		],
+		"effects": {
+			"engineering": +1,
+			"curiosity": +1
+		}
+	},
+	"What kinds of engineers are there?": {
+		"text": [
+			'You and Mary go in together and ask the teacher "What kind of engineers are there?"',
+			"The teacher tells you there are all kinds, ones that work with chemicals, electricity, infrastructure, machines, and many more."
+		],
+		"effects": {
+			"engineering": +1,
+			"curiosity": +1
+		}
+	},
+	"Are you an engineer?": {
+		"text": '"I don\'t know about that one... I think our teacher is a [i]teacher[/i], not an engineer. Thanks for trying, though."'
+	},
+	"How do you become an engineer?": {
+		"text": [
+			'You and Mary go in together and ask the teacher "How do you become an engineer?"',
+			"The teacher explains that engineers need lots of knowledge and training to make things for people to use. Most engineers go to college and get a degree."
+		],
+		"effects": {
+			"engineering": +1,
+			"curiosity": +1
+		}
+	}
+}
