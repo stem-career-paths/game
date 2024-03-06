cc400612b1b75cf81b4b7b84cc9605e7f120a800
Jeffrey Harmon <contact@jeffreyharmon.dev>
2024-03-06 16:24:04 -0500

    Add StoryLoader and unit tests

---
+++ project/common/story_loader.gd
@@ -0,0 +1,64 @@
+class_name StoryLoader
+extends RefCounted
+
+const STORY_PATH := "res://story/"
+const STARTING_STORIES_PATH := STORY_PATH + "starting_stories/"
+
+
+## Get all story paths in the starting stories directory
+func get_starting_story_paths() -> Array[String]:
+	return get_story_paths_in_directory(STARTING_STORIES_PATH)
+
+
+## Get all simple story paths in the starting stories directory
+func get_starting_simple_story_paths() -> Array[String]:
+	return get_simple_story_paths_in_directory(STARTING_STORIES_PATH)
+
+
+## Get all story paths in a directory
+func get_story_paths_in_directory(path: String) -> Array[String]:
+	var paths: Array[String] = []
+
+	var dir := DirAccess.open(path)
+	if not dir:
+		return paths
+
+	dir.list_dir_begin()
+	var file_name := dir.get_next()
+
+	while file_name != "":
+		var file_path := path.path_join(file_name)
+		print("Checking file: " + file_path)
+		if dir.current_is_dir():
+			paths.append_array(get_story_paths_in_directory(file_path))
+		elif _is_valid_story_path(file_path):
+			paths.append(file_path)
+
+		file_name = dir.get_next()
+
+	return paths
+
+
+## Get all simple story paths in a directory
+func get_simple_story_paths_in_directory(path: String) -> Array[String]:
+	return get_story_paths_in_directory(path).filter(
+		func(story_path: String) -> bool: return load_simple_story(story_path) != null
+	)
+
+
+## Load a simple story from a file path
+func load_simple_story(file_path: String) -> SimpleStory:
+	if not _is_valid_story_path(file_path):
+		return null
+
+	var resource := load(file_path)
+	var story: Object = resource.new()
+
+	if not story is SimpleStory:
+		return null
+
+	return story
+
+
+func _is_valid_story_path(path: String) -> bool:
+	return path.ends_with(".gd") and FileAccess.file_exists(path)
---
+++ project/test/unit/story_loader_test.gd
@@ -0,0 +1,67 @@
+extends GutTest
+
+const TEST_STORY_DIR := "res://test/unit/story_loader_test_stories/"
+
+var story_loader: StoryLoader
+
+
+func before_each() -> void:
+	story_loader = StoryLoader.new()
+
+
+func test_get_story_paths_in_directory() -> void:
+	var path := TEST_STORY_DIR.path_join("all_gd")
+	var story_paths = story_loader.get_story_paths_in_directory(path)
+	assert_eq(story_paths.size(), 2)
+
+
+func test_get_story_paths_in_directory_with_non_gd_files() -> void:
+	var path := TEST_STORY_DIR.path_join("not_all_gd")
+	var story_paths = story_loader.get_story_paths_in_directory(path)
+	assert_eq(story_paths.size(), 2)
+
+
+func test_get_story_paths_in_directory_with_no_stories() -> void:
+	var path := TEST_STORY_DIR.path_join("no_stories")
+	var story_paths = story_loader.get_story_paths_in_directory(path)
+	assert_eq(story_paths.size(), 0)
+
+
+func test_get_story_paths_in_directory_with_invalid_directory() -> void:
+	var path := TEST_STORY_DIR.path_join("invalid_directory")
+	var story_paths = story_loader.get_story_paths_in_directory(path)
+	assert_eq(story_paths.size(), 0)
+
+
+func test_get_simple_story_paths_in_directory() -> void:
+	var path := TEST_STORY_DIR.path_join("all_gd")
+	var story_paths = story_loader.get_simple_story_paths_in_directory(path)
+	assert_eq(story_paths.size(), 1)
+
+
+func test_load_simple_story_valid_story() -> void:
+	var file_path := TEST_STORY_DIR.path_join("all_gd").path_join("test_story_1.gd")
+	var story := story_loader.load_simple_story(file_path)
+
+	assert_not_null(story)
+
+
+func test_load_simple_story_invalid_story() -> void:
+	var file_path := TEST_STORY_DIR.path_join("all_gd").path_join("test_story_2.gd")
+	var story = story_loader.load_simple_story(file_path)
+
+	assert_null(story)
+
+
+func test_load_simple_story_non_gd_file() -> void:
+	var file_path := TEST_STORY_DIR.path_join("not_all_gd").path_join("not_a_story.txt")
+	var story = story_loader.load_simple_story(file_path)
+
+	assert_null(story)
+
+
+func test_load_simple_story_invalid_file_path() -> void:
+	var file_path := TEST_STORY_DIR.path_join("invalid_directory").path_join("invalid_story.gd")
+	var story = story_loader.load_simple_story(file_path)
+
+	assert_null(story)
