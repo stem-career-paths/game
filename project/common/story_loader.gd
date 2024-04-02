42e8403e89c6a419f7e1bee54e981ddf6c124140
Jeffrey Harmon <contact@jeffreyharmon.dev>
2024-04-02 18:23:20 -0400

    Add the ability to ignore directories in story_loader
    Ignores "res://story/demo_stories/" by default

--- project/common/story_loader.gd
+++ project/common/story_loader.gd
@@ -1,8 +1,14 @@
 class_name StoryLoader
 extends RefCounted
 
+## Where the story files are located
 const STORY_PATH := "res://story/"
+
+## Where the starting stories are located
 const STARTING_STORIES_PATH := STORY_PATH + "starting_stories/"
+
+## Paths to ignore when loading stories
+const DEFAULT_IGNORED_PATHS: Array[String] = [ STORY_PATH + "demo_stories/" ]
 
 
 ## Get all story paths in the starting stories directory
@@ -16,7 +22,7 @@ func get_starting_simple_story_paths() -> Array[String]:
 
 
 ## Get all story paths in a directory
-func get_story_paths_in_directory(path: String) -> Array[String]:
+func get_story_paths_in_directory(path: String, ignored_paths: Array[String] = DEFAULT_IGNORED_PATHS) -> Array[String]:
 	var paths: Array[String] = []
 
 	var dir := DirAccess.open(path)
@@ -30,8 +36,8 @@ func get_story_paths_in_directory(path: String) -> Array[String]:
 		var file_path := path.path_join(file_name)
 
 		if dir.current_is_dir():
-			paths.append_array(get_story_paths_in_directory(file_path))
-		elif _is_valid_story_path(file_path):
+			paths.append_array(get_story_paths_in_directory(file_path, ignored_paths))
+		elif _is_valid_story_path(file_path, ignored_paths):
 			paths.append(file_path)
 
 		file_name = dir.get_next()
@@ -40,15 +46,15 @@ func get_story_paths_in_directory(path: String) -> Array[String]:
 
 
 ## Get all simple story paths in a directory
-func get_simple_story_paths_in_directory(path: String) -> Array[String]:
-	return get_story_paths_in_directory(path).filter(
-		func(story_path: String) -> bool: return load_simple_story(story_path) != null
+func get_simple_story_paths_in_directory(path: String, ignored_paths: Array[String] = DEFAULT_IGNORED_PATHS) -> Array[String]:
+	return get_story_paths_in_directory(path, ignored_paths).filter(
+		func(story_path: String) -> bool: return load_simple_story(story_path, ignored_paths) != null
 	)
 
 
 ## Load a simple story from a file path
-func load_simple_story(file_path: String) -> SimpleStory:
-	if not _is_valid_story_path(file_path):
+func load_simple_story(file_path: String, ignored_paths: Array[String] = []) -> SimpleStory:
+	if not _is_valid_story_path(file_path, ignored_paths):
 		return null
 
 	var resource := load(file_path)
@@ -60,5 +66,13 @@ func load_simple_story(file_path: String) -> SimpleStory:
 	return story
 
 
-func _is_valid_story_path(path: String) -> bool:
-	return path.ends_with(".gd") and FileAccess.file_exists(path)
+func _is_valid_story_path(path: String, ignored_paths: Array[String]) -> bool:
+	return not _is_ignored_story_path(path, ignored_paths) and path.ends_with(".gd") and FileAccess.file_exists(path)
+
+
+func _is_ignored_story_path(path: String, ignored_paths: Array[String]) -> bool:
+	for ignored_path in ignored_paths:
+		if path.begins_with(ignored_path):
+			return true
+
+	return false
--- project/test/unit/story_loader_test.gd
+++ project/test/unit/story_loader_test.gd
@@ -39,10 +39,30 @@ func test_get_story_paths_in_directory_with_invalid_directory() -> void:
 	assert_eq(story_paths.size(), 0)
 
 
+func test_get_story_paths_with_ignore() -> void:
+	var path := TEST_STORY_DIR
+	var ignore: Array[String] = [ TEST_STORY_DIR.path_join("all_gd") ]
+
+	var all_story_paths = story_loader.get_story_paths_in_directory(path)
+	var ignore_story_paths = story_loader.get_story_paths_in_directory(path, ignore)
+
+	assert_ne(all_story_paths.size(), ignore_story_paths.size())
+
+
 func test_get_simple_story_paths_in_directory() -> void:
 	var path := TEST_STORY_DIR.path_join("all_gd")
 	var story_paths = story_loader.get_simple_story_paths_in_directory(path)
 	assert_eq(story_paths.size(), 1)
+
+
+func test_get_simple_story_paths_with_ignore() -> void:
+	var path := TEST_STORY_DIR
+	var ignore: Array[String] = [ TEST_STORY_DIR.path_join("all_gd") ]
+
+	var all_story_paths = story_loader.get_simple_story_paths_in_directory(path)
+	var ignore_story_paths = story_loader.get_simple_story_paths_in_directory(path, ignore)
+
+	assert_ne(all_story_paths.size(), ignore_story_paths.size())
 
 
 func test_load_simple_story_valid_story() -> void:
@@ -69,5 +89,14 @@ func test_load_simple_story_non_gd_file() -> void:
 func test_load_simple_story_invalid_file_path() -> void:
 	var file_path := TEST_STORY_DIR.path_join("invalid_directory").path_join("invalid_story.gd")
 	var story = story_loader.load_simple_story(file_path)
+
+	assert_null(story)
+
+
+func test_load_simple_story_ignore_returns_null() -> void:
+	var file_path := TEST_STORY_DIR.path_join("all_gd").path_join("test_story_1.gd")
+	var ignore: Array[String] = [ TEST_STORY_DIR.path_join("all_gd") ]
+
+	var story = story_loader.load_simple_story(file_path, ignore)
 
 	assert_null(story)
