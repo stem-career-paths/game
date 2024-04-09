5cc2fb856c1bf02ef3c7b791bacdb5589fe53eb0
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-04-09 10:11:54 -0400

    Improve layout

--- project/ui/epilogue/epilogue_summary.gd
+++ project/ui/epilogue/epilogue_summary.gd
@@ -9,31 +9,34 @@ func _ready() -> void:
 		var label := _make_summary_label()
 		label.text = "Took a gap year"
 		label.type = SummaryItem.Type.TIME
-		add_child(label)
+		_add_summary_item(label)
 	
 	if epilogue.community_college:
 		var label := _make_summary_label()
 		label.text = "Attended Community College"
 		label.type = SummaryItem.Type.INSTITUTION
-		add_child(label)
+		_add_summary_item(label)
 
 	var college_label := _make_summary_label()
 	college_label.text = "Attended %s" % Epilogue.as_string(epilogue.college_type)
 	college_label.type = SummaryItem.Type.INSTITUTION
-	add_child(college_label)
+	_add_summary_item(college_label)
 
 	for major in epilogue.majors:
 		var label := _make_summary_label()
 		label.text = "Major: %s" % major.capitalize()
 		label.type = SummaryItem.Type.DEGREE
-		add_child(label)
+		_add_summary_item(label)
 	
 	for minor in epilogue.minors:
 		var label := _make_summary_label()
 		label.text = "Minor: %s" % minor.capitalize()
 		label.type = SummaryItem.Type.DEGREE
-		add_child(label)
+		_add_summary_item(label)
 
+
+func _add_summary_item(item : Control) -> void:
+	%SummaryItemsBox.add_child(item)
 
 func _make_summary_label() -> Control:
 	return preload("res://ui/epilogue/summary_item.tscn").instantiate()
