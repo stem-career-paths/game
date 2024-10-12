81e197d298c5744010c882a3b5000c04a8495d00
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-10-12 07:16:45 -0400

    Replace carousel with crossfade cast preview
    The carousel caused problems on iOS. It seems to be related to this:
    https://github.com/godotengine/godot/issues/95677
    
    Working around it by replacing the feature seemed the most prudent course.

---
+++ project/ui/cast_preview.gd
@@ -0,0 +1,24 @@
+extends Control
+
+## How long is the crossfade, in seconds
+@export var crossfade_speed := 0.5
+
+## The child index that is currently shown
+var _index := 0
+
+@onready var _cast_previews : Array[Node] = %Cast.get_children()
+
+
+func _ready() -> void:
+	_cast_previews.shuffle()
+	for i in _cast_previews.size():
+		if i != _index:
+			_cast_previews[i].modulate.a = 0
+
+
+func _on_visible_duration_timeout() -> void:
+	var current := _cast_previews[_index]
+	create_tween().tween_property(current, "modulate:a", 0, crossfade_speed)
+	_index = (_index + 1) % _cast_previews.size()
+	var next := _cast_previews[_index]
+	create_tween().tween_property(next, "modulate:a", 1, crossfade_speed)
